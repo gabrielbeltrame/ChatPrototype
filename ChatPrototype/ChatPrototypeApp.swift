cf38c674c4e4be83da4565dabe8d082705f7ef3b
gabriel.beltrame <gabriel.beltrame@totvs.com.br>
2024-06-12 15:37:00 -0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Gabriel Beltrame Silva on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
